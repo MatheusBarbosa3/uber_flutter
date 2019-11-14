66dd70a8a1f25c6645176a35fef55200850cdecc
MatheusBarbosa3 <matheusbdos31@gmail.com>
2019-11-13 21:18:04 -0300

    registration screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:uber_flutter/screens/Home.dart';
 
+final ThemeData defaultTheme = ThemeData(
+    primaryColor: Color(0xff37474f),
+    accentColor: Color(0xff546e7a)
+);
+
 void main() => runApp(MaterialApp(
   title: "Uber",
   home: Home(),
+  theme: defaultTheme,
   debugShowCheckedModeBanner: false,
 ));
+
+
+
+
+
 
--- lib/screens/Cadastro.dart
+++ lib/screens/Cadastro.dart
@@ -1,43 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:uber_flutter/screens/Cadastro.dart';
 
-class Home extends StatefulWidget {
+class Cadastro extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _CadastroState createState() => _CadastroState();
 }
 
-class _HomeState extends State<Home> {
+class _CadastroState extends State<Cadastro> {
 
+  TextEditingController _controllerNome = TextEditingController();
   TextEditingController _controllerEmail = TextEditingController();
   TextEditingController _controllerSenha = TextEditingController();
+  bool _tipoUsuarioPassageiro = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Cadastro"),
+      ),
       body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("images/fundo.png"),
-                fit: BoxFit.cover
-            )
-        ),
         padding: EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 32),
-                  child: Image.asset(
-                    "images/logo.png",
-                    width: 200,
-                    height: 150,
+                TextField(
+                  controller: _controllerNome,
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "nome completo",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
                   ),
                 ),
                 TextField(
                   controller: _controllerEmail,
-                  autofocus: true,
                   keyboardType: TextInputType.emailAddress,
                   style: TextStyle(fontSize: 20),
                   decoration: InputDecoration(
@@ -66,10 +70,27 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Text("Passageiro"),
+                      Switch(
+                          value: _tipoUsuarioPassageiro,
+                          onChanged: (bool valor){
+                            setState(() {
+                              _tipoUsuarioPassageiro = valor;
+                            });
+                          }
+                      ),
+                      Text("Motorista"),
+                    ],
+                  ),
+                ),
+                Padding(
                   padding: EdgeInsets.only(top: 16, bottom: 10),
                   child: RaisedButton(
                       child: Text(
-                        "Entrar",
+                        "Cadastrar",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                       color: Color(0xff1ebbd8),
@@ -77,22 +98,6 @@ class _HomeState extends State<Home> {
                       onPressed: (){
 
                       }
-                  ),
-                ),
-                Center(
-                  child: GestureDetector(
-                    child: Text(
-                      "Não tem conta? cadastre-se!",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onTap: (){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Cadastro()
-                          )
-                      );
-                    },
                   ),
                 ),
                 Padding(
