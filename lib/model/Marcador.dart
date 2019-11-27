45aae665913092cb7e6d0847d85d1fa85c243dba
MatheusBarbosa3 <matheusbdos31@gmail.com>
2019-11-27 01:48:13 -0300

    displaying passenger destination

---
+++ lib/model/Marcador.dart
@@ -0,0 +1,13 @@
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Marcador {
+
+  LatLng local;
+  String caminhoImagem;
+  String titulo;
+
+  Marcador(this.local, this.caminhoImagem, this.titulo);
+
+
+}
